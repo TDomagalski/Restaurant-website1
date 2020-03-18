890bc3b41ce4e43a686ed591634c9c4b485e9e8c
Tomasz <tomekwebdev@gmail.com>
2020-03-18 15:24:12 +0100

    menu dropdown done

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
+// hamburger icon
+
 const nav = document.querySelector("#navigation"),
     navWrap = nav.firstElementChild
-btnHam = nav.lastElementChild;
+btnHam = nav.lastElementChild,
+    btnHamIcon = btnHam.firstElementChild;
 
 btnHam.addEventListener("click", function showNav() {
     navWrap.classList.toggle("showNav");
     btnHam.classList.toggle("btn-hamburger_left");
+    btnHamIcon.classList.toggle("btnHamIcon");
 }, false)
+
+const arrow = document.querySelector(".clickArrow"),
+    menuDrop = document.querySelector(".menu-drop");
+
+arrow.addEventListener("click", function () {
+    menuDrop.classList.toggle("menu-dropClic");
+}, false);
